fd92f02df207e2abc146962d1e01907eff944979
Svetlana Vrabie <laurasdamir@gmail.com>
2025-01-02 20:54:01 -0500

    refactor: small changes;

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -36,7 +36,7 @@ const Nav = () => {
           <a href="/">Menu</a>
         </li>
         <li>
-          <a href="/">Reservations</a>
+          <a href="/booking">Reservations</a>
         </li>
         <li>
           <a href="/">Order Online</a>
